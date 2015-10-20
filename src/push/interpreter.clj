94978b14ccb80aaa337d2c413167269dd8b068d7
Bill Tozier <bill@vagueinnovation.com>
2015-10-20 08:08:01 -0400

    docstrig for make-interpreter, core-stacks

--- src/push/interpreter.clj
+++ src/push/interpreter.clj
@@ -3,18 +3,25 @@
 
 (defrecord Interpreter [program stacks])
 
-(def core-stacks {:boolean '()
-                  :char '()
-                  :code '()
-                  :input '()
-                  :integer '() 
-                  :exec '()
-                  :float '()
-                  :string '()
-                  })
+
+(def core-stacks
+  "the basic types central to the Push language"
+    {:boolean '()
+    :char '()
+    :code '()
+    :input '()
+    :integer '() 
+    :exec '()
+    :float '()
+    :string '()
+    })
+
 
 (defn make-interpreter
-  "creates a new Interpreter record"
+  "creates a new Interpreter record
+  If no arguments are given, the Interpreter has an empty :program and ony core :stacks;
+  if a program is given, it is stored;
+  if a hashmap of stack values is given, that is merged onto the core empty stacks."
   ([] (make-interpreter []))
   ([program] (->Interpreter program core-stacks))
   ([program stacks] (->Interpreter program (merge core-stacks stacks)))
