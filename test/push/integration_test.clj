f03e1226466b91e65e1929160d736061ea126d6c
Bill Tozier <bill@vagueinnovation.com>
2015-10-20 14:45:01 -0400

    added integration test

---
+++ test/push/integration_test.clj
@@ -0,0 +1,13 @@
+(ns push.integration-test
+  (:use midje.sweet)
+  (:use [push.interpreter]))
+
+(future-fact "simple scripts containing literals sort them as expected" :integration
+  (all-stacks (run-script [1 2.3 false \T "foo" '(1 2 3)])) => 
+    { :integer '(1)
+      :float '(2.3)
+      :boolean '(false)
+      :char '(\T)
+      :string '("foo")
+      :code '( '(1 2 3) )}
+  )
