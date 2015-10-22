10b34aa8842cfbcf9aa6d60e060db7551265ad00
Bill Tozier <bill@vagueinnovation.com>
2015-10-22 07:34:04 -0400

    simplified design spike by eliminating DSL wrapper object

--- test/push/design_spikes/dsl_macros.clj
+++ test/push/design_spikes/dsl_macros.clj
@@ -15,6 +15,7 @@
 ;;
 ; - `count-of [stackname :as local]`
 ;    store the number of items in `stackname` under key `local`
+;    raise an Exception if the stack doesn't exist
 ; - `consume [stackname]`
 ;    pop an item (and discard it) from `stackname`
 ;    raise an Exception if it's empty or undefined
@@ -45,6 +46,8 @@
 ; - `replace-stack [stackname new-stack]`
 ;    replace the indicated stack with a new list
 ;    raise an exception if it's undefined
+; - `calculate [fn :as local]
+;    save the result of `fn` under key `local`
 
 ;; And I THINK that supports all defined Push instructions I've ever seen....
 
@@ -61,52 +64,87 @@
   ([interpreter locals] (->PushDslState interpreter locals)))
 
 
+(defn store-local
+  "adds a local kv pair to an Interpreter's :local map"
+  [interpreter k v]
+  (if (nil? (:locals interpreter))
+    (assoc interpreter :locals {k v})
+    (assoc-in interpreter [:locals k] v)))
+
+
+(fact "store-local creates a :locals store if needed"
+  (:locals (make-interpreter)) => nil
+  (:locals (store-local (make-interpreter) :foo 8)) => {:foo 8})
+
+
+(fact "store-local amends an existing :locals store"
+  (let [foo (store-local (make-interpreter) :foo 8)]
+  (:locals (store-local foo :bar 9)) => {:foo 8, :bar 9}))
+
+
+(fact "store-local can overwrite an existing :locals value"
+  (let [foo (store-local (make-interpreter) :foo 8)]
+  (:locals (store-local foo :foo 999)) => {:foo 999}))
+
+
+(defn clear-locals
+  "deletes the :locals key from an Interpreter"
+  [interpreter]
+  (dissoc interpreter :locals))
+
+
+(fact "clear-locals returns a thing that's still an Interpreter"
+  (class (clear-locals (make-interpreter))) => push.interpreter.Interpreter
+  (let [foo (store-local (make-interpreter) :foo 8)]
+    (class (clear-locals foo)) => push.interpreter.Interpreter))
+
+
+(fact "clear-locals also does what it's supposed to do"
+  (let [foo (store-local (make-interpreter) :foo 8)]
+    (:locals (clear-locals foo)) => nil))
+
+
 (defn consume
-  [dslstep stack & {:keys [as]}]
-  (let [interpreter (:interpreter dslstep)
-        old-stack (get-stack interpreter stack)
-        old-locals (:locals dslstep)]
+  [interpreter stack & {:keys [as]}]
+  (let [old-stack (get-stack interpreter stack)]
     (if (empty? old-stack)
       (throw (Exception. (str "Push DSL Error: " stack " is empty")))
-      (make-PushDslState
+      (store-local
         (set-stack interpreter stack (pop old-stack))
-        (assoc old-locals as (first old-stack)))
-    )))
+          as
+          (first old-stack)))
+      ))
 
 
-(def sixier (make-PushDslState six-ints))
-
-
-(fact "consume returns a new PushDslState with a changed :interpreter and new locals"
-  (:locals (consume sixier :integer :as :int1)) => {:int1 6}
-  (get-stack (:interpreter (consume sixier :integer :as :int1)) :integer) => '(5 4 3 2 1))
+(fact "consume returns an :interpreter with new locals"
+  (:locals (consume six-ints :integer :as :int1)) => {:int1 6}
+  (get-stack (consume six-ints :integer :as :int1) :integer) => '(5 4 3 2 1))
 
 
 (fact "consume throws an exception if the stack is empty"
-  (:locals (consume sixier :boolean :as :nope)) => (throws #"Push DSL Error:"))
+  (:locals (consume six-ints :boolean :as :nope)) => (throws #"Push DSL Error:"))
 
 
 (fact "consume can be thread-firsted"
-  (let [two-popped (->  sixier
+  (let [two-popped (->  six-ints
                         (consume :integer :as :int1)
                         (consume :integer :as :int2))]
     (:locals two-popped) => {:int1 6, :int2 5}
-    (get-stack (:interpreter two-popped) :integer) => '(4 3 2 1)))
+    (get-stack two-popped :integer) => '(4 3 2 1)))
 
 
 (defn place
-  [dslstep stack args function]
-  (let [result (apply function (map (:locals dslstep) args))
-        interpreter (:interpreter dslstep)]
-    (assoc dslstep :interpreter (push-item interpreter stack result))))
+  [interpreter stack args function]
+  (let [result (apply function (map (:locals interpreter) args))]
+    (push-item interpreter stack result)))
 
 
 (fact "`place` will apply the specified inline function to the dslstate locals named"
-  (let [integer-add (-> sixier
+  (let [integer-added (-> six-ints
                         (consume :integer :as :int1)
                         (consume :integer :as :int2)
                         (place :integer [:int1 :int2] #(+ %1 %2)))]
-  (get-stack (:interpreter integer-add) :integer) => '(11 4 3 2 1))) ;; 
+  (get-stack integer-added :integer) => '(11 4 3 2 1))) ;; 
 
 
 ; (def-pushinstruction
