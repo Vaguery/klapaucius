5b73aa9f540872c45ce50501116d54c54baabf97
Bill Tozier <bill@vagueinnovation.com>
2015-11-16 14:08:40 -0500

    cleaned out failed codox experiment

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject push "0.0.1-SNAPSHOT"
   :description "Push language interpreter"
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[lein-codox "0.9.0"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
   
