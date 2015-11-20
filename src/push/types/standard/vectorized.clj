ae345422621eeac629854cf768ba10c5914e597c
Bill Tozier <bill@vagueinnovation.com>
2015-11-20 17:16:17 -0500

    several :collectable instructions

--- src/push/types/standard/vectorized.clj
+++ src/push/types/standard/vectorized.clj
@@ -9,16 +9,9 @@
 
 ;; TODO
 
-; vector_X_conj
 ; vector_X_take
 ; vector_X_subvec
-; vector_X_first
-; vector_X_last
 ; vector_X_nth
-; vector_X_rest
-; vector_X_butlast
-; vector_X_length
-; vector_X_reverse
 ; vector_X_pushall
 ; vector_X_emptyvector
 ; vector_X_contains
@@ -29,6 +22,19 @@
 ; vector_X_replacefirst
 ; vector_X_remove
 ; exec_do*vector_X
+
+
+(defn x-butlast-instruction
+  [typename]
+  (let [instruction-name (str (name typename) "-butlast")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1] #(butlast %1) :as :most)
+      `(push.instructions.dsl/push-onto ~typename :most)))))
 
 
 (defn x-concat-instruction
@@ -46,6 +52,86 @@
       `(push.instructions.dsl/push-onto ~typename :concatted)))))
 
 
+(defn x-conj-instruction
+  [typename rootname]
+  (let [instruction-name (str (name typename) "-conj")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~rootname :as :arg2)
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1 :arg2]
+          #(conj %1 %2) :as :conjed)
+      `(push.instructions.dsl/push-onto ~typename :conjed)))))
+
+
+(defn x-first-instruction
+  [typename rootname]
+  (let [instruction-name (str (name typename) "-first")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1] #(first %1) :as :top)
+      `(push.instructions.dsl/push-onto ~rootname :top)))))
+
+
+(defn x-last-instruction
+  [typename rootname]
+  (let [instruction-name (str (name typename) "-last")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1] #(last %1) :as :tail)
+      `(push.instructions.dsl/push-onto ~rootname :tail)))))
+
+
+(defn x-length-instruction
+  [typename]
+  (let [instruction-name (str (name typename) "-length")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1] #(count %1) :as :len)
+      `(push.instructions.dsl/push-onto :integer :len)))))
+
+
+(defn x-rest-instruction
+  [typename]
+  (let [instruction-name (str (name typename) "-rest")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1] #(into [] (rest %1)) :as :end)
+      `(push.instructions.dsl/push-onto ~typename :end)))))
+
+
+(defn x-reverse-instruction
+  [typename]
+  (let [instruction-name (str (name typename) "-reverse")]
+    (eval (list
+      'push.instructions.core/build-instruction
+      instruction-name
+      "DOCSTRING"
+      :tags #{:vector}
+      `(push.instructions.dsl/consume-top-of ~typename :as :arg1)
+      '(push.instructions.dsl/calculate [:arg1] #(reverse %1) :as :bw)
+      `(push.instructions.dsl/push-onto ~typename :bw)))))
+
+
 (defn vector-of-type?
   [item type]
   (let [checker (:recognizer type)]
@@ -56,7 +142,8 @@
 (defn build-vectorized-type
   "creates a vector [sub]type for another Push type"
   [content-type]
-  (let [typename (keyword (str (name (:name content-type)) "s"))]
+  (let [typename (keyword (str (name (:name content-type)) "s"))
+        rootname (keyword (name (:name content-type)))]
     ( ->  (t/make-type  typename
                         :recognizer #(vector-of-type? % content-type)
                         :attributes #{:vector})
@@ -65,5 +152,13 @@
           t/make-movable
           print/make-printable
           env/make-returnable
-          (t/attach-instruction , (x-concat-instruction typename)))))
+          (t/attach-instruction , (x-butlast-instruction typename))
+          (t/attach-instruction , (x-concat-instruction typename))
+          (t/attach-instruction , (x-conj-instruction typename rootname))
+          (t/attach-instruction , (x-first-instruction typename rootname))
+          (t/attach-instruction , (x-last-instruction typename rootname))
+          (t/attach-instruction , (x-length-instruction typename))
+          (t/attach-instruction , (x-rest-instruction typename))
+          (t/attach-instruction , (x-reverse-instruction typename))
+          )))
 
--- test/push/instructions/standard/vectorized_test.clj
+++ test/push/instructions/standard/vectorized_test.clj
@@ -19,12 +19,167 @@
     (contains [:foos-concat :foos-dup] :in-any-order :gaps-ok))
 
 
+(tabular
+  (fact "`foos-butlast` pushes the butlast of the first :foos vector back onto :foos"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos   '([1 2 3])
+     :foo    '(9.9)}          :foos-butlast     {:foos   '([1 2])
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([])
+     :foo    '(9.9)}          :foos-butlast     {:foos   '()   ;;; NOTE nil
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([1 2 3])
+     :foo    '()}             :foos-butlast     {:foos   '([1 2])
+                                                 :foo    '()}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    )
+
+
 
 (tabular
-  (fact ""
+  (fact "`foos-concat` concatenates the top two foos vectors"
     (register-type-and-check-instruction
         ?set-stack ?items foos-type ?instruction ?get-stack) => ?expected)
 
     ?set-stack  ?items                 ?instruction      ?get-stack   ?expected
     :foos       '([3 4] [1 2])         :foos-concat      :foos       '([1 2 3 4])
+    :foos       '([] [1 2])            :foos-concat      :foos       '([1 2])
+    :foos       '([3 4] [])            :foos-concat      :foos       '([3 4])
+    :foos       '([] [])               :foos-concat      :foos       '([])
+    :foos       '([1 2 3])             :foos-concat      :foos       '([1 2 3])
+    )
+
+
+(tabular
+  (fact "`foos-conj` conjes the top :foo item onto the top :foos vector"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos   '([1 2 3])
+     :foo    '(9.9)}          :foos-conj        {:foos   '([1 2 3 9.9])
+                                                 :foo    '()}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([])
+     :foo    '(9.9)}          :foos-conj        {:foos   '([9.9])
+                                                 :foo    '()}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([1 2 3])
+     :foo    '()}             :foos-conj        {:foos   '([1 2 3])
+                                                 :foo    '()}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    )
+
+
+(tabular
+  (fact "`foos-first` pushes the first item of the top :foos vector onto :foo"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos   '([1 2 3])
+     :foo    '(9.9)}          :foos-first       {:foos   '()
+                                                 :foo    '(1 9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([])
+     :foo    '(9.9)}          :foos-first       {:foos   '()   ;;; NOTE nil
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([1 2 3])
+     :foo    '()}             :foos-first       {:foos   '()
+                                                 :foo    '(1)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    )
+
+
+
+(tabular
+  (fact "`foos-last` pushes the last item of the top :foos vector onto :foo"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos   '([1 2 3])
+     :foo    '(9.9)}          :foos-last        {:foos   '()
+                                                 :foo    '(3 9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([])
+     :foo    '(9.9)}          :foos-last        {:foos   '()   ;;; NOTE nil
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([1 2 3])
+     :foo    '()}             :foos-last        {:foos   '()
+                                                 :foo    '(3)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    )
+
+
+(tabular
+  (fact "`foos-length` pushes the length of the top :foos vector onto :integer"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos     '([1 2 3])
+     :integer  '()}           :foos-length        {:foos    '()
+                                                   :integer '(3)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos     '([1 2])
+     :integer  '()}           :foos-length        {:foos    '()
+                                                   :integer '(2)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos     '([])
+     :integer  '()}           :foos-length        {:foos    '()
+                                                   :integer '(0)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    )
+
+(tabular
+  (fact "`foos-rest` pushes the rest of the first :foos vector back onto :foos"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos   '([1 2 3])
+     :foo    '(9.9)}          :foos-rest       {:foos   '([2 3])
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([])
+     :foo    '(9.9)}          :foos-rest       {:foos   '([])    ;;; NOTE not nil!
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([1 2 3])
+     :foo    '()}             :foos-rest       {:foos   '([2 3])
+                                                 :foo    '()}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    )
+
+
+
+(tabular
+  (fact "`foos-reverse` pushes the reverse of the first :foos vector back onto :foos"
+    (check-instruction-with-all-kinds-of-stack-stuff
+        ?new-stacks foos-type ?instruction) => (contains ?expected))
+
+    ?new-stacks                ?instruction     ?expected
+
+    {:foos   '([1 2 3])
+     :foo    '(9.9)}          :foos-reverse       {:foos   '([3 2 1])
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    {:foos   '([])
+     :foo    '(9.9)}          :foos-reverse       {:foos   '([])  
+                                                 :foo    '(9.9)}
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     )
