0adfc20fa462b86b37b8e4fc162d0d23f0c7d825
Bill Tozier <bill@vagueinnovation.com>
2015-11-19 19:15:18 -0500

    upgrade to midje-1.8.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject push "0.0.1-SNAPSHOT"
   :description "Push language interpreter"
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.2"]]}})
   
   
