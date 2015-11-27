2d397e01ab0eca991d85e78950d8d4a2321da856
Bill Tozier <bill@vagueinnovation.com>
2015-11-27 17:45:38 -0500

    :set-intersection, :set-difference, :set-subset?, :set-superset?

--- src/push/types/extra/set.clj
+++ src/push/types/extra/set.clj
@@ -11,6 +11,41 @@
   )
 
 
+(def set-difference
+  (t/simple-2-in-1-out-instruction
+    "`:set-difference` pops the top two `:set` items, and pushes their difference"
+    :set "difference" 'sets/difference))
+
+
+(def set-intersection
+  (t/simple-2-in-1-out-instruction
+    "`:set-intersection` pops the top two `:set` items, and pushes their intersection"
+    :set "intersection" 'sets/intersection))
+
+
+(def set-subset?
+  (core/build-instruction
+    set-subset?
+    "`:set-subset?` pops the top two `:float` values (call them `A` and `B`, respectively). Pushes `true` if `B` is a subset of `A`, `false` otherwise."
+    :tags #{:set :predicate}
+    (d/consume-top-of :set :as :b)
+    (d/consume-top-of :set :as :a)
+    (d/calculate [:a :b]
+      #(sets/subset? %2 %1) :as :well?)
+    (d/push-onto :boolean :well?)))
+
+
+(def set-superset?
+  (core/build-instruction
+    set-superset?
+    "`:set-superset?` pops the top two `:float` values (call them `A` and `B`, respectively). Pushes `true` if `B` is a superset of `A`, `false` otherwise."
+    :tags #{:set :predicate}
+    (d/consume-top-of :set :as :b)
+    (d/consume-top-of :set :as :a)
+    (d/calculate [:a :b]
+      #(sets/superset? %2 %1) :as :well?)
+    (d/push-onto :boolean :well?)))
+
 
 (def set-union
   (t/simple-2-in-1-out-instruction
@@ -29,6 +64,10 @@
       make-movable
       make-printable
       make-returnable
+      (t/attach-instruction , set-difference)
+      (t/attach-instruction , set-intersection)
+      (t/attach-instruction , set-subset?)
+      (t/attach-instruction , set-superset?)
       (t/attach-instruction , set-union)
       )))
 
