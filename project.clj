d4881650582e3e06bd5195d1ff9b66e360cb5bee
Bill Tozier <bill@vagueinnovation.com>
2015-12-01 20:33:04 -0500

    converted midje example to gorilla repl .cljw file

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject push "0.0.1-SNAPSHOT"
   :description "Push language interpreter"
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-gorilla "0.3.5"]]
   :profiles {:dev {:dependencies [[midje "1.8.2"]]}})
   
   
