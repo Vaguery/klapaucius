a3f3fd18042d285556b1236e4f4a5f1a076bce57
Bill Tozier <bill@vagueinnovation.com>
2015-12-06 11:54:28 -0500

    copied gorilla code over to demo.examples.plane-geometry.*

---
+++ src/demo/examples/plane_geometry/definitions.clj
@@ -0,0 +1,158 @@
+(ns demo.examples.plane-geometry.definitions
+  (:require [push.interpreter.core :as core])
+  (:require [push.types.core :as t])
+  (:require [push.instructions.core :as i])
+  (:require [push.instructions.dsl :as d])
+  (:require [push.instructions.aspects :as aspects])
+  )
+
+
+;;; points
+
+
+(defrecord Point [x y])
+
+(defn make-point
+  "Builds a Point record from two numbers"
+  [x y]
+  (->Point x y))
+
+
+;;; lines
+
+(defrecord Line [p1 p2])
+
+(defn make-line
+  "Builds an (oriented) Line record from two Points that lie on that line; throws an Exception if the point arguments are equal"
+  [a b]
+  (if (= a b)
+    (throw (Exception. "make-line argument error: points must differ"))
+    (->Line a b)))
+
+
+;;; circles
+
+(defrecord Circle [origin edgepoint])
+
+(defn make-circle
+  "Builds a Circle record from two numbers, one the origin and one lying on its circumference; throws an Exception if the point arguments are equal"
+  [origin edge]
+  (if (= origin edge)
+    (throw (Exception. "make-circle argument error: points must differ"))
+    (->Circle origin edge)))
+
+
+;;; helpers
+
+(defn slope
+  "returns the slope of a Push `:line` item, or the keyword `:infinity` if the line is vertical"
+  [line]
+  (let [x1 (double (:x (:p1 line)))
+        y1 (double (:y (:p1 line)))
+        x2 (double (:x (:p2 line)))
+        y2 (double (:y (:p2 line)))]
+    (if (= x1 x2)
+      :infinity
+      (/ (- y2 y1) (- x2 x1)))))
+
+
+(defn intercept
+  "returns the y-intercept of a Push `:line` item, or nil if the line is vertical"
+  [line]
+  (let [x1 (double (:x (:p1 line)))
+        y1 (double (:y (:p1 line)))
+        x2 (double (:x (:p2 line)))
+        y2 (double (:y (:p2 line)))]
+    (if (= x1 x2)
+      nil
+      (- y1 (* x1 (slope line))) ;; y - mx = b
+      )))
+  
+
+(defn line-coincide?
+  "returns `true` if the lines have the same canonical equation"
+  [line1 line2]
+  (and
+    (= (slope line1) (slope line2))
+    (= (intercept line1) (intercept line2))))
+
+
+(defn line-at-x
+  "returns the y value at a given x for a line argument"
+  [line x]
+  (+ (intercept line) (* (slope line) x)))
+
+
+(defn crossing-point
+  "returns a `point` where its two `line` arguments intersect, if they intersect at all (even if one is vertical)"
+  [line1 line2]
+  (let [s1 (slope line1)
+        i1 (intercept line1)
+        s2 (slope line2)
+        i2 (intercept line2)]
+  (cond (= s1 s2)
+        nil
+        (= s1 :infinity)
+          (let [x (:x (:p1 line1))]
+            (make-point x (line-at-x line2 x)))
+        (= s2 :infinity)
+          (let [x (:x (:p1 line2))]
+            (make-point x (line-at-x line1 x)))
+        :else
+        (let [f (/ (- i2 i1) (- s1 s2))]
+            (make-point f (+ i1 (* s1 f)))))))
+
+
+;;; push instructions
+
+
+(def crosspoint
+  (i/build-instruction
+    line-intersection
+    "`:line-intersection` pops the top two `:line` items, and pushes the `:point` at which they intersect, if they are not the same or parallel"
+    :tags #{:plane-geometry :construction}
+    (d/consume-top-of :line :as :arg2)
+    (d/consume-top-of :line :as :arg1)
+    (d/calculate [:arg1 :arg2] #(crossing-point %1 %2) :as :pt)
+    (d/push-onto :point :pt)))
+
+
+
+;;; push types
+
+  (def push-point
+    (-> (t/make-type 
+          :point 
+          :recognizer #(instance? Point %))
+        aspects/make-visible
+        aspects/make-equatable
+        aspects/make-movable
+        aspects/make-printable
+        aspects/make-quotable
+        aspects/make-returnable))
+
+    
+(def push-line
+  (-> (t/make-type 
+        :line 
+        :recognizer #(instance? Line %))
+      aspects/make-visible
+      aspects/make-equatable
+      aspects/make-movable
+      aspects/make-printable
+      aspects/make-quotable
+      aspects/make-returnable
+      (t/attach-instruction crosspoint)))
+
+
+
+(def push-circle
+  (-> (t/make-type 
+        :circle 
+        :recognizer #(instance? Circle %))
+      aspects/make-visible
+      aspects/make-equatable
+      aspects/make-movable
+      aspects/make-printable
+      aspects/make-quotable
+      aspects/make-returnable))
---
+++ src/demo/examples/plane_geometry/test.clj
@@ -0,0 +1,191 @@
+(ns demo.examples.plane-geometry.test
+  (:use midje.sweet)
+  (:require [push.interpreter.templates.one-with-everything :as owe])
+  (:require [push.interpreter.core :as core])
+  (:require [push.util.stack-manipulation :as u])
+  (:use demo.examples.plane-geometry.definitions)
+  )
+
+
+;; constructors
+
+
+(fact "I can make and recognize instances of :points, :lines and :circles"
+  ((:recognizer push-point) (make-point 3 9))         => true
+  ((:recognizer push-point) 9912)                     => false
+
+  ((:recognizer push-line)
+     (make-line (make-point 2 9) (make-point 2 1)))   => true
+  ((:recognizer push-line)
+     (make-point 2 9))                                => false
+
+  ((:recognizer push-circle)
+     (make-circle (make-point 2 9) (make-point 2 1))) => true
+  ((:recognizer push-circle)
+     (make-point 2 9))                                => false
+  ((:recognizer push-circle)
+     (make-line (make-point 2 9) (make-point 2 1)))   => false)
+
+
+(fact "`make-line` throws an Exception if both point arguments are the same"
+  (make-line (make-point 1 2) (make-point 1 2)) => (throws #"points must differ"))
+
+
+(fact "`make-circle` throws an Exception if both point arguments are the same"
+  (make-circle (make-point 1 2) (make-point 1 2)) => (throws #"points must differ")
+  (make-circle (make-point 1 2.000002) (make-point 1 2.000001)) =not=> (throws))
+
+
+;; recognizers work for routing
+
+
+(def test-interpreter
+  (core/register-types 
+    (owe/make-everything-interpreter)
+    [push-point push-line push-circle]))
+
+
+(def point-class (class (make-point 0.0 0.0)))
+
+
+(def line-class (class (make-line (make-point 0.0 0.0) (make-point 1 2))))
+
+
+(def circle-class (class (make-circle (make-point 0.0 0.0) (make-point 1 2))))
+
+(fact "the stacks are alredy there (though it doesn't really matter)"
+  (keys (:stacks test-interpreter)) => (contains [:circle :line :point] :gaps-ok :any-order))
+
+
+(fact "items can be routed to those stacks correctly"
+  (u/get-stack
+    (core/handle-item test-interpreter (make-point 3 9))
+    :point) => (list (map->Point {:x 3, :y 9}))
+
+  (u/get-stack
+    (core/handle-item test-interpreter (make-line (make-point 2 9) (make-point 2 1)))
+    :line) => (list (map->Line { :p1 (map->Point {:x 2, :y 9})
+                                 :p2 (map->Point {:x 2, :y 1}) }))
+
+  (u/get-stack
+    (core/handle-item test-interpreter (make-circle (make-point 2 9) (make-point 2 1)))
+    :circle) => (list (map->Circle {
+                    :origin
+                      (map->Point {:x 2, :y 9})
+                    :edgepoint
+                      (map->Point {:x 2, :y 1})})))
+
+
+;; helpers
+
+  
+(fact "slope works as expected"
+  (slope (make-line (make-point 2.0 2.0) (make-point 1.0 1.0))) => 1.0
+  (slope (make-line (make-point 1.0 0.0) (make-point 0.0 1.0))) => -1.0
+  (slope (make-line (make-point 1.0 3.0) (make-point 2.0 1.0))) => -2.0
+  (slope (make-line (make-point 2.0 1.0) (make-point 1.0 3.0))) => -2.0
+  (slope (make-line (make-point 1.0 1.0) (make-point 0.0 1.0))) => 0.0)
+
+
+(fact "slope returns the keyword :infinity if the line is vertical"
+  (slope (make-line (make-point 0.0 0.0) (make-point 0.0 1.0))) => :infinity)
+
+
+(fact "slope can detect subtle differences"
+  (slope (make-line (make-point 3.111 2.9128) (make-point 0 0))) =not=>
+    (slope (make-line (make-point 0 0) (make-point 3.112 2.9128))))
+
+
+(fact "intercept works as expected"
+  (intercept (make-line (make-point 2.0 2.0) (make-point 1.0 1.0))) => 0.0
+  (intercept (make-line (make-point 1.0 0.0) (make-point 0.0 1.0))) => 1.0
+  (intercept (make-line (make-point 1.0 0.0) (make-point -1.0 -1.0))) => -0.5
+  (intercept (make-line (make-point 1.0 3.0) (make-point 2.0 1.0))) => 5.0
+  (intercept (make-line (make-point 2.0 1.0) (make-point 1.0 3.0))) => 5.0
+  (intercept (make-line (make-point 1.0 1.0) (make-point 0.0 1.0))) => 1.0)
+
+
+(fact "intercept returns nil if the line is vertical"
+  (intercept (make-line (make-point 0.0 0.0) (make-point 0.0 1.0))) => nil)
+
+
+(fact "intercept can detect subtle differences"
+  (intercept (make-line (make-point 3.111 2.9128) (make-point 0 0))) =not=>
+    (intercept (make-line (make-point 0 0.0001) (make-point 3.111 2.9128))))
+
+
+;; line-coincide?
+
+
+(fact "`line-coincide` should detect 'reversed' lines"
+  (line-coincide?
+    (make-line (make-point 0 2) (make-point 0 11))
+    (make-line (make-point 0 11) (make-point 0 2))) => true
+  (line-coincide?
+    (make-line (make-point 3.111 2.9128) (make-point 0 0))
+    (make-line (make-point 0 0) (make-point 3.111 2.9128))) => true)
+
+
+(fact "`line-coincide` should be able to detect subtle differences"
+  (line-coincide?
+    (make-line (make-point 0 2.0) (make-point 1 11))
+    (make-line (make-point 1 11) (make-point 0 2.00001))) => false)
+
+
+(fact "`line-coincide` isn't thrown off by type rounding errors"
+  ;; this came up along the way
+  (line-coincide?
+    (make-line (make-point 0 2.0) (make-point 1 11))
+    (make-line (make-point 1 11.0) (make-point 2 20))) => true
+  (line-coincide?
+    (make-line (make-point 0 2.0) (make-point 1 11))
+    (make-line (make-point 1 11) (make-point 2 20))) => true)
+
+
+;; 
+
+
+(def line1 (make-line (make-point 0.0 1.0) (make-point 1.0 2.0)))
+(def line2 (make-line (make-point 0.0 10.0) (make-point 5.0 0.0)))
+(def line3 (make-line (make-point 6.0 10.0) (make-point 6.0 0.0)))
+(def line4 (make-line (make-point 0.0 5.0) (make-point 10.0 5.0)))
+
+
+(fact "crossing-point returns the point where lines cross, if they do"
+  (crossing-point line1 line2) => (make-point 3.0 4.0)
+  (crossing-point line1 line3) => (make-point 6.0 7.0)
+  (crossing-point line1 line4) => (make-point 4.0 5.0)
+  (crossing-point line2 line3) => (make-point 6.0 -2.0)
+  (crossing-point line2 line4) => (make-point 2.5 5.0)
+  (crossing-point line3 line4) => (make-point 6.0 5.0))
+
+
+(fact "crossing-point returns nil if they don't intersect"
+  (crossing-point line1 line1) => nil
+  (crossing-point line3 (make-line (make-point 3.0 0.0) (make-point 3.0 12.0))) => nil
+  (crossing-point line1 (make-line (make-point -1.0 0.0) (make-point 0.0 1.0))) => nil
+  (crossing-point line4 (make-line (make-point 0.0 0.0) (make-point 3.0 0.0))) => nil)
+
+(fact "crossing-point is pretty accurate even for small divergences"
+  (crossing-point line1 (make-line (make-point 0.0 1.0) (make-point 112230.0 112231.0001))) => (make-point 0.0 1.0))
+
+
+
+(def test-interpreter
+  (core/register-types 
+    (owe/make-everything-interpreter)
+    [push-point push-line push-circle]))
+
+
+(fact "test-interpreter knows :line-intersection"
+  (keys (:instructions test-interpreter)) => (contains :line-intersection))
+
+
+(fact "the intersection ends up on the :point stack"
+  (let [prepped (core/recycle-interpreter
+                test-interpreter
+                [line1 line2 :line-intersection])]
+    (:stacks prepped) =>
+    `{:boolean (), :booleans (), :char (), :chars (), :circle (), :code (), :environment (), :error (), :exec (~line1 ~line2 :line-intersection), :float (), :floats (), :integer (), :integers (), :line (), :log (), :point (), :print (), :return (), :set (), :string (), :strings (), :unknown (), :vector ()}
+    
+    (u/get-stack (core/run prepped 1000) :point) => (list (make-point 3.0 4.0))))
