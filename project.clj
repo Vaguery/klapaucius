a54aaf45bde260a81dbfcb4b808d3008bb0f7fbd
Bill Tozier <bill@vagueinnovation.com>
2016-01-11 17:55:14 -0500

    modified defproject group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject push-in-clojure "0.1.0-SNAPSHOT"
+(defproject org.clojars.vaguery/push-in-clojure "0.1.0-SNAPSHOT"
   :description "Push language interpreter"
   :url "https://github.com/Vaguery/push-in-clojure"
   :dependencies [[org.clojure/clojure            "1.7.0"]
