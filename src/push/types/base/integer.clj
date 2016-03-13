37e56c5f78435bdf51c864446568255835281921
Bill Tozier <bill@vagueinnovation.com>
2016-03-13 12:11:23 -0400

    cleaned up :integer type

--- src/push/types/base/integer.clj
+++ src/push/types/base/integer.clj
@@ -19,7 +19,6 @@
 ;; INSTRUCTIONS
 
 
-
 (def integer-abs (t/simple-1-in-1-out-instruction
   "`:integer-abs` pushes the absolute value of the top `:integer` item"
   :integer "abs" 'math/abs))
@@ -41,32 +40,6 @@
     (d/calculate [:arg]
       #(into '() (reverse (exotics/integer-to-truth-table (math/abs %1) 1))) :as :bits)
     (d/push-onto :exec :bits)))
-
-
-
-(def integer->bits
-  (core/build-instruction
-    integer->bits
-    "`:integer->bits` pops the top `:integer` and pushes a vector of its 'bits' (as `true` and `false` in low-to-high order) to the `:booleans` stack. Negative numbers are made positive. The minimal binary representation is used, but always at least two bits are produced."
-    :tags #{:numeric :conversion}
-    (d/consume-top-of :integer :as :arg)
-    (d/calculate [:arg]
-      #(exotics/integer-to-truth-table (math/abs %1) 1) :as :bits)
-    (d/push-onto :booleans :bits)))
-
-
-
-
-(def integer->numerals
-  (core/build-instruction
-    integer->numerals
-    "`:integer->numerals` pops the top `:integer` and pushes a vector of its numerals (not including negative sign or bigint decorations) to `:chars` "
-    :tags #{:numeric :conversion}
-    (d/consume-top-of :integer :as :arg)
-    (d/calculate [:arg]
-      #(into [] (filter #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9} (seq (str %1)))) :as :numbers)
-    (d/push-onto :chars :numbers)))
-
 
 
 
@@ -134,6 +107,7 @@
     (d/push-onto :integer :scaled)))
 
 
+
 (def integer-many
   (core/build-instruction
     integer-many
@@ -142,6 +116,7 @@
     (d/consume-top-of :integer :as :arg)
     (d/calculate [:arg] #(mod %1 1000) :as :scaled)
     (d/push-onto :integer :scaled)))
+
 
 
 (def integer-mod
@@ -157,13 +132,14 @@
     (d/push-these-onto :integer [:replacement :remainder])
     (d/calculate [:denominator]
       #(if (zero? %1) ":integer-mod 0 denominator" nil) :as :warning)
-    (d/record-an-error :from :warning)
-    ))
+    (d/record-an-error :from :warning)))
+
 
 
 (def integer-multiply (t/simple-2-in-1-out-instruction
   "`:integer-multiply` pops the top two `:integer` items, and pushes their product"
   :integer  "multiply" '*'))
+
 
 
 (def integer-sign (t/simple-1-in-1-out-instruction
@@ -182,66 +158,10 @@
     (d/push-onto :integer :scaled)))
 
 
+
 (def integer-subtract (t/simple-2-in-1-out-instruction
   "`:integer-subtract` pops the top two `:integer` items, and pushes their difference, subtracting the top from the second" :integer "subtract" '-'))
 
-
-;; conversion
-
-
-(def boolean->integer
-  (core/build-instruction
-    boolean->integer
-    "`:boolean->integer` pops the top `:boolean`. If it's `true`, it pushes 1; if `false`, it pushes 0."
-    :tags #{:base :conversion}
-    (d/consume-top-of :boolean :as :arg1)
-    (d/calculate [:arg1] #(if %1 1 0) :as :logic)
-    (d/push-onto :integer :logic)))
-
-
-(def boolean->signedint
-  (core/build-instruction
-    boolean->signedint
-    "`:boolean->signedint` pops the top `:boolean`. If it's `true`, it pushes 1; if `false`, it pushes -1."
-    :tags #{:base :conversion}
-    (d/consume-top-of :boolean :as :arg1)
-    (d/calculate [:arg1] #(if %1 1 -1) :as :logic)
-    (d/push-onto :integer :logic)))
-
-
-(def float->integer
-  (core/build-instruction
-    float->integer
-    "`:float->integer` pops the top `:float` item, and converts it to an `:integer` value (using CLojure's `bigint` function)"
-    :tags #{:numeric :base :conversion}
-    (d/consume-top-of :float :as :arg1)
-    (d/calculate [:arg1] #(bigint %1) :as :int)
-    (d/push-onto :integer :int)))
-
-
-(def char->integer
-  (core/build-instruction
-    char->integer
-    "`:char->integer` pops the top `:char` item, and converts it to an (integer) index"
-    :tags #{:base :conversion}
-    (d/consume-top-of :char :as :arg1)
-    (d/calculate [:arg1] #(long %1) :as :int)
-    (d/push-onto :integer :int)))
-
-
-(def string->integer
-  (core/build-instruction
-    string->integer
-    "`:string->integer` pops the top `:string` item, and applies `Long/parseLong` to attempt to convert it to a fixed-point value. If successful (that is, if no exception is raised), the result is pushed to `:integer`"
-    :tags #{:conversion :base :numeric}
-    (d/consume-top-of :string :as :arg)
-    (d/calculate [:arg] 
-      #(try (Long/parseLong %1) (catch NumberFormatException _ nil))
-        :as :result)
-    (d/push-onto :integer :result)))
-
-
-;; exotic
 
 
 (def integer-totalistic3
@@ -254,6 +174,88 @@
     (d/push-onto :integer :result)))
 
 
+
+;; CONVERSIONS
+
+
+(def boolean->integer
+  (core/build-instruction
+    boolean->integer
+    "`:boolean->integer` pops the top `:boolean`. If it's `true`, it pushes 1; if `false`, it pushes 0."
+    :tags #{:base :conversion}
+    (d/consume-top-of :boolean :as :arg1)
+    (d/calculate [:arg1] #(if %1 1 0) :as :logic)
+    (d/push-onto :integer :logic)))
+
+
+
+(def boolean->signedint
+  (core/build-instruction
+    boolean->signedint
+    "`:boolean->signedint` pops the top `:boolean`. If it's `true`, it pushes 1; if `false`, it pushes -1."
+    :tags #{:base :conversion}
+    (d/consume-top-of :boolean :as :arg1)
+    (d/calculate [:arg1] #(if %1 1 -1) :as :logic)
+    (d/push-onto :integer :logic)))
+
+
+
+(def char->integer
+  (core/build-instruction
+    char->integer
+    "`:char->integer` pops the top `:char` item, and converts it to an (integer) index"
+    :tags #{:base :conversion}
+    (d/consume-top-of :char :as :arg1)
+    (d/calculate [:arg1] #(long %1) :as :int)
+    (d/push-onto :integer :int)))
+
+
+
+(def float->integer
+  (core/build-instruction
+    float->integer
+    "`:float->integer` pops the top `:float` item, and converts it to an `:integer` value (using CLojure's `bigint` function)"
+    :tags #{:numeric :base :conversion}
+    (d/consume-top-of :float :as :arg1)
+    (d/calculate [:arg1] #(bigint %1) :as :int)
+    (d/push-onto :integer :int)))
+
+
+
+(def integer->bits
+  (core/build-instruction
+    integer->bits
+    "`:integer->bits` pops the top `:integer` and pushes a vector of its 'bits' (as `true` and `false` in low-to-high order) to the `:booleans` stack. Negative numbers are made positive. The minimal binary representation is used, but always at least two bits are produced."
+    :tags #{:numeric :conversion}
+    (d/consume-top-of :integer :as :arg)
+    (d/calculate [:arg]
+      #(exotics/integer-to-truth-table (math/abs %1) 1) :as :bits)
+    (d/push-onto :booleans :bits)))
+
+
+
+(def integer->numerals
+  (core/build-instruction
+    integer->numerals
+    "`:integer->numerals` pops the top `:integer` and pushes a vector of its numerals (not including negative sign or bigint decorations) to `:chars` "
+    :tags #{:numeric :conversion}
+    (d/consume-top-of :integer :as :arg)
+    (d/calculate [:arg]
+      #(into [] (filter #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9} (seq (str %1)))) :as :numbers)
+    (d/push-onto :chars :numbers)))
+
+
+
+(def string->integer
+  (core/build-instruction
+    string->integer
+    "`:string->integer` pops the top `:string` item, and applies `Long/parseLong` to attempt to convert it to a fixed-point value. If successful (that is, if no exception is raised), the result is pushed to `:integer`"
+    :tags #{:conversion :base :numeric}
+    (d/consume-top-of :string :as :arg)
+    (d/calculate [:arg] 
+      #(try (Long/parseLong %1) (catch NumberFormatException _ nil))
+        :as :result)
+    (d/push-onto :integer :result)))
 
 
 (def integer-type
