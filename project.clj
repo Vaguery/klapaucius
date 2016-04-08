2950600094e16dba9e44192dd7aba865c4c30332
Bill Tozier <bill@vagueinnovation.com>
2016-04-08 13:59:31 -0400

    bumped version again to 0.1.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klapaucius "0.1.14-SNAPSHOT"
+(defproject klapaucius "0.1.15-SNAPSHOT"
   :description "Push language interpreter"
   :url "https://github.com/Vaguery/klapaucius"
   :dependencies [[org.clojure/clojure            "1.8.0"]
