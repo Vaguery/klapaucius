27cac7c14ab67874e10eaec54f62ff5a88587457
Bill Tozier <bill@vagueinnovation.com>
2017-03-27 05:31:27 -0400

    some cleanup

--- test/acceptance/push_prison.clj
+++ test/acceptance/push_prison.clj
@@ -23,7 +23,7 @@
   (-> (make-everything-interpreter
         :config {:step-limit 20000
                  :lenient true
-                 :max-collection-size 32768}
+                 :max-collection-size 131072}
         :bindings (:bindings prisoner)
         :program (:program prisoner))
       reset-interpreter))
@@ -52,15 +52,15 @@
           (if (is-done? s)
             (println "DONE")
             (recur (do
-              (println (str "\n>>> " (:counter s)
-                            "\nstacks (pts): " (fix/count-collection-points (:stacks s))
-                            "\nbindings (pts): " (fix/count-collection-points (:bindings s))
-                            "\nall items in :log (sum of chars): "
-                            (apply (juxt max +')
-                              (into '(0)
-                                (map
-                                  #(count (str (:item %)))
-                                  (u/get-stack s :log))))
+              (println (str ">>> " (:counter s)
+                            ; "\nstacks (pts): " (fix/count-collection-points (:stacks s))
+                            ; "\nbindings (pts): " (fix/count-collection-points (:bindings s))
+                            ; "\nall items in :log (sum of chars): "
+                            ; (apply (juxt max +')
+                            ;   (into '(0)
+                            ;     (map
+                            ;       #(count (str (:item %)))
+                            ;       (u/get-stack s :log))))
                             ; "\n items on :exec " (u/get-stack s :exec)
                             ; "\n>>> ATTEMPTING " (pr-str (first (u/get-stack s :exec)))
                             ; "\n items on :OUTPUT " (get-in s [:bindings :OUTPUT] '())
@@ -86,12 +86,17 @@
    'push.type.definitions.quoted.QuotedCode map->QuotedCode
     })
 
+(def prisoner-names
+  (file-seq (clojure.java.io/file "test/acceptance/prisoners")))
+
+
 (def prisoners
   [
-    (edn/read-string {:readers edn-readers}
-                     (slurp "test/acceptance/prisoners/e445321c-22bf-45b2-acce-3aac6acffe61.txt"))
+    ; (edn/read-string {:readers edn-readers}
+    ;                  (slurp "test/acceptance/prisoners/0d18a279-1a6b-4585-b239-3c9c927d2023.txt"))
+    ; (edn/read-string {:readers edn-readers}
+    ;                  (slurp  "test/acceptance/prisoners/e445321c-22bf-45b2-acce-3aac6acffe61.txt"))
   ])
-
 
 (fact "no exceptions are raised when I step through any of these problematic programs"
   :debug :acceptance
