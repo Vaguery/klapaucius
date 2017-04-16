2d795d1d0f57ba199ca453b28ff870d7113d18b2
Bill Tozier <bill@vagueinnovation.com>
2017-04-16 11:44:14 -0400

    removed unused dependencies from project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/Vaguery/klapaucius"
   :dependencies [[org.clojure/clojure            "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.apfloat/apfloat            "1.8.2"]
-                 [marick/structural-typing       "2.0.5"]
                  [com.climate/claypoole          "1.1.4"]
                  [inflections                   "0.13.0"]
                  [dire                           "0.5.4"]
